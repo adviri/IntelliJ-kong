d6168d2f78a6d4b22a5c6e0dc3feee97e86ddba8
GitHub Action <action@github.com>
2021-08-22 09:59:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/adviri/intellijkong/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/adviri/intellijkong/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.adviri.intellijkong.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adviri.intellijkong.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/adviri/intellijkong/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adviri/intellijkong/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.adviri.intellijkong.services
 
+import com.github.adviri.intellijkong.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
